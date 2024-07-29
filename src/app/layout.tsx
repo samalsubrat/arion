a17d079351e3b8c33e3736413b6647cfa759408f
Subrat Samal <samalsubrat919@gmail.com>
2024-07-29 21:15:26 +0530

    speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -28,6 +29,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <SpeedInsights/>
           <Footer/>
         </ThemeProvider>
       </body>
